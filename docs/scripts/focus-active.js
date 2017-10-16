a644c248c1349d1a2e9757ed40d5ad4da6fc67d0
joshstar <joshstar@mail.com>
2017-10-16 11:22:52 +0100

    Update reference docs

--- docs/scripts/focus-active.js
+++ docs/scripts/focus-active.js
@@ -1,8 +1,15 @@
 (function () {
-    var navScroll = document.getElementById('navication-scroll');
-    var header = document.querySelector('nav header');
-    var active = document.querySelector('.slds-is-active a');
 
-    if(active)
-        navScroll.scrollTop = active.offsetTop - header.offsetHeight - Math.ceil(active.offsetHeight / 2)
+    var autofocus = document.querySelector('[autofocus]');
+    var actives = document.querySelectorAll('.slds-is-active a');
+    var len = actives.length;
+    var i = 0;
+
+    while (i < len) {
+        actives[i].focus();
+        i++;
+    }
+
+    if (autofocus)
+        autofocus.focus();
 })()
